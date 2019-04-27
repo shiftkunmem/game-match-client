92618e62ae7a632c4008e0919f5e6d6fdaf9c341
yutomakino <mazinai.tarazu@gmail.com>
2019-04-27 19:28:01 +0900

    Test react

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -1,19 +1,23 @@
 import * as React from 'react';
 import './App.css';
 
-import logo from './logo.svg';
+class App extends React.Component <{}, { data: string | [], apiUrl: string }> {
+  constructor(props: {}) {
+    super(props);
+    this.state = {data: [], apiUrl: 'https://api.game-match.net/user'};
+  }
 
-class App extends React.Component {
+  public componentDidMount() {
+    fetch(this.state.apiUrl)
+      .then(res => res.json())
+      .then(resJson => this.setState({data: resJson.user.toString()}))
+      .catch((error) => console.error(error))
+  }
+
   public render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.tsx</code> and save to reload.
-        </p>
+        {this.state.data}
       </div>
     );
   }
